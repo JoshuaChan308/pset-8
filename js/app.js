0549af1c5729e2f732e1fe48841a6683720f2643
JoshuaChan308 <55455749+JoshuaChan308@users.noreply.github.com>
2020-02-01 16:28:39 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+let win;
+
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+
+///////////////////// EVENT LISTENERS ///////////////////////////////
+
+window.onload = init;
+
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("switch").onclick = switch_turn;
