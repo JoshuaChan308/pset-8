367a1349ed182d3c33522007e74cb1900a168452
JoshuaChan308 <55455749+JoshuaChan308@users.noreply.github.com>
2020-02-08 16:00:34 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,22 +15,20 @@ let board;
 let turn;
 let win;
 let determine_first_player;
-let x_wins_count = 0;
-let o_wins_count = 0;
-
+let x_wins = 0;
+let o_wins = 0;
+let ties= 0
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 const squares = Array.from(document.querySelectorAll("#board div"));
 const message = document.querySelector("h2");
 
+
 ///////////////////// EVENT LISTENERS ///////////////////////////////
-
 window.onload = init;
-
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
-
-
+document.getElementById("reset-scoreboard").onclick = resetScoreboard;
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function init() {
   board = [
@@ -39,5 +37,21 @@ function init() {
     "", "", ""
   ];
 
-  
-  
+  do {
+    var first_Player = prompt("Enter X or O to declare the first player: ");
+    if (first_Player === null) {
+      turn = "X";
+      break;
+    } else if (first_Player === "X" || first_Player === "x") {
+      turn = "X";
+    } else if (first_Player === "O" || first_Player === "o") {
+      turn = "O";
+    } else {
+      determine_first_player = 0;
+    }
+  } while (first_Player !== "X" && first_Player !== "x" && first_Player !== "O" && first_Player !== "o");
+
+  win = null;
+
+  render();
+}
